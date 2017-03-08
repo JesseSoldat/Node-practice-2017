3d2c26c0452d69895c028f0b672d578fd46ee28c
jesse <jsoldat@hotmail.com>
2017-03-08 08:19:35 +0700

    working with global object

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hello ninjas');
+// console.log(global);
+var fileName = __filename; //full path to file inclued file name
+var dirName = __dirname; //full path to directory
+//console.log('FILENAME: ',fileName);
+//console.log('DIRNAME: ',dirName);
+
+var time = 0;
+
+// setTimeout(function(){
+// 	console.log('3 seconds has passed');
+// },3000);
+
+var timer = setInterval(function(){
+	time += 2;
+	console.log(time + ' seconds has passed');
+
+	if(time >= 10){
+		clearInterval(timer);
+	}
+},2000);
+
+
+
